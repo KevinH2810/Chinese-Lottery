85ea826f317487da32036b7824bde4b39c901956
Kevin Hadi <47102992+KevinH2810@users.noreply.github.com>
2022-05-04 22:04:46 +0800

    clean code

--- lottery.sol
+++ lottery.sol
@@ -11,33 +11,15 @@ contract Lottery is VRFConsumerBaseV2 {
     bool private locked;
     VRFCoordinatorV2Interface COORDINATOR;
 
-    // Your subscription ID.
     uint64 s_subscriptionId;
-
-    // Rinkeby coordinator. For other networks,
-    // see https://docs.chain.link/docs/vrf-contracts/#configurations
     address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
-
-    // The gas lane to use, which specifies the maximum gas price to bump to.
-    // For a list of available gas lanes on each network,
-    // see https://docs.chain.link/docs/vrf-contracts/#configurations
     bytes32 keyHash =
         0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
-    // Depends on the number of requested values that you want sent to the
-    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-    // so 100,000 is a safe default for this example contract. Test and adjust
-    // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
     uint32 callbackGasLimit = 100000;
 
-    // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 3;
-
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
-    uint32 numWords = 2;
+    uint16 requestConfirmations = 6;.
+    uint32 numWords = 1;
 
     uint256 internal s_randomWords;
     uint256 internal s_requestId;
