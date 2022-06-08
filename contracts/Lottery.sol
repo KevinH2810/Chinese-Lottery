3f4bce5489d11053e73c7a43dbbb1c19115818ad
KevinH2810 <kevinhadisutanto@gmail.com>
2022-06-09 05:45:06 +0800

    update smart contract with better state

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+// import "github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/ChainlinkClient.sol";
 
 contract Lottery is VRFConsumerBaseV2 {
+    enum LOTTERY_STATE { OPEN, CLOSED, SELECTING_WINNER}
+    LOTTERY_STATE private lottery_state; 
     address payable[] public players;
     address public manager;
     address payable winner;
-    bool private locked;
     VRFCoordinatorV2Interface COORDINATOR;
 
     uint64 s_subscriptionId;
-    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    //this is rinkeby LINK contract
+    // address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
     bytes32 keyHash =
         0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
@@ -27,14 +30,24 @@ contract Lottery is VRFConsumerBaseV2 {
 
     event setupRandomNumber(uint[] randomWord, uint settleds_randomWords);
 
-    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+    constructor(address vrfCoordinator, bytes32 _keyhash) VRFConsumerBaseV2(vrfCoordinator) {
+        // setPublicChainlinkToken();
+        keyHash = _keyhash;
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         manager = msg.sender;
-        s_subscriptionId = subscriptionId;
-        locked = false;
+        // s_subscriptionId = subscriptionId;
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
-    receive() external payable isNotLocked {
+//     constructor(address vrfCoordinator, address link, bytes32 _keyhash, uint256 _fee)
+// VRFConsumerBase(vrfCoordinator, link)
+// {
+//    keyHash = _keyhash;
+//    fee = _fee;
+//    admin = msg.sender;
+// }
+
+    receive() external payable isOpen {
         require(msg.value == 0.001 ether, "Only transfer 0.001");
         players.push(payable(msg.sender));
     }
@@ -46,14 +59,11 @@ contract Lottery is VRFConsumerBaseV2 {
     function chooseWinner()
         public
         onlyOwner
-        isLocked
+        isSelectingWinner
         playerExist
         returns (address)
     {
-        require(
-            winner == address(0),
-            "winner has been choosen, please re start the bet"
-        );
+        lottery_state = LOTTERY_STATE.CLOSED;
 
         winner = players[s_randomWords];
         // winner = players[randomNumber % 37];
@@ -61,16 +71,17 @@ contract Lottery is VRFConsumerBaseV2 {
         return winner;
     }
 
-    function startNewBid() public onlyOwner isLocked {
+    function startNewBid() public onlyOwner isClosed {
         // re set variables
+        lottery_state = LOTTERY_STATE.OPEN;
         winner = payable(address(0));
         players = new address payable[](0);
-        locked = false;
         s_requestId = 0;
     }
 
     // Assumes the subscription is funded sufficiently.
-    function requestRandomWords() external onlyOwner playerExist isNotLocked {
+    function requestRandomWords() external onlyOwner playerExist isOpen {
+        lottery_state = LOTTERY_STATE.SELECTING_WINNER;
         // require(s_requestId == 0, "already request randomWords");
         // locked = true;
         // Will revert if subscription is not set and funded.
@@ -87,33 +98,38 @@ contract Lottery is VRFConsumerBaseV2 {
     function fulfillRandomWords(
         uint256, /* requestId */
         uint256[] memory randomWords
-    ) internal override isLocked {
+    ) internal override isSelectingWinner {
         s_randomWords = (randomWords[0] % players.length);
         // s_randomWords = randomWords ;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == manager);
+        require(msg.sender == manager, "Only Owner Can Access");
         _;
     }
 
     modifier onlyWinnerAndOwner() {
-        require(msg.sender == manager || msg.sender == winner);
+        require(msg.sender == manager || msg.sender == winner, "Only Owner and Current Winner can access");
         _;
     }
 
-    modifier isNotLocked() {
-        require(locked == false, "Bet is locked");
+    modifier isOpen() {
+        require(lottery_state == LOTTERY_STATE.OPEN, "Bet is not open");
         _;
     }
 
-    modifier isLocked() {
-        require(locked == true, "Bet is Unlocked");
+    modifier isClosed() {
+        require(lottery_state == LOTTERY_STATE.CLOSED, "Bet is not closed yet");
         _;
     }
+
+    modifier isSelectingWinner() {
+      require(lottery_state == LOTTERY_STATE.SELECTING_WINNER, "Bet isn't selecting winner yet");
+      _;
+  }
 
     modifier playerExist() {
-        require(players.length > 0);
+        require(players.length > 0, "No Players");
         _;
     }
 }
